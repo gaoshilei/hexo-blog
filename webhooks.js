e7a789e99ec5e9f5c93130714af69e0032aca750
gaoshilei <731351288@qq.com>
2017-11-03 12:06:52 +0800

    update

---
+++ webhooks.js
@@ -0,0 +1,11 @@
+var http = require('http')
+var exec = require('child_process').exec
+
+http.createServer(function (req, res) {
+    // 该路径与WebHooks中的路径部分需要完全匹配，实现简易的授权认证。
+    if(req.url === '/webhooks/push/666666'){
+        // 如果url匹配，表示认证通过，则执行 sh ./deploy.sh
+        exec('sh ./deploy.sh')
+    }
+    res.end()
+}).listen(4000)
