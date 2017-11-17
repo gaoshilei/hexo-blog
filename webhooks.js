d1d74edd60b2a349ea52748ab3cbffb004ba7050
gaoshilei <731351288@qq.com>
2017-11-17 18:47:27 +0800

    update

--- webhooks.js
+++ webhooks.js
@@ -1,35 +1,11 @@
 var http = require('http')
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhooks_push', secret: 'leonlei1226' })
-// 上面的 secret 保持和 GitHub 后台设置的一致
-
-function run_cmd(cmd, args, callback) {
-  var spawn = require('child_process').spawn;
-  var child = spawn(cmd, args);
-  var resp = "";
-
-  child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
-  child.stdout.on('end', function() { callback (resp) });
-}
-
-handler.on('error', function (err) {
-  console.error('Error:', err.message)
-})
-
-handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref);
-    run_cmd('sh', ['./deploy.sh'], function(text){ console.log(text) });
-})
-
-try {
-  http.createServer(function (req, res) {
-    handler(req, res, function (err) {
-      res.statusCode = 404
-      res.end('no such location')
-    })
-  }).listen(6666)
-} catch(e) {
-  console.error('Error:', e.message)
-}
+var exec = require('child_process').exec
+ 
+http.createServer(function (req, res) {
+// 该路径与WebHooks中的路径部分需要完全匹配，实现简易的授权认证。
+  if(req.url === '/webhooks_push'){
+  // 如果url匹配，表示认证通过，则执行 sh ./deploy.sh
+  exec('sh ./deploy.sh')
+  }
+  res.end()
+}).listen(6666)
