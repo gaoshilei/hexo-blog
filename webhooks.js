1d1a4ac1e78e6f4ebce2588efaaf098d50f5b42e
gaoshilei <731351288@qq.com>
2017-11-17 17:08:12 +0800

    update

--- webhooks.js
+++ webhooks.js
@@ -1,45 +1,35 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhooks_push', secret: 'leonlei1226' }) 
+var handler = createHandler({ path: '/webhooks_push', secret: 'leonlei1226' })
 // 上面的 secret 保持和 GitHub 后台设置的一致
- 
+
 function run_cmd(cmd, args, callback) {
   var spawn = require('child_process').spawn;
   var child = spawn(cmd, args);
   var resp = "";
- 
+
   child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
   child.stdout.on('end', function() { callback (resp) });
 }
- 
-try {
-  http.createServer(function (req, res) {
-    handler(req, res, function (err) {
-      res.statusCode = 404
-      res.end('no such location')
-    })
-  }).listen(6666);
-} catch(error) {
-    console.error('Error:', error.message)
-}
- 
+
 handler.on('error', function (err) {
   console.error('Error:', err.message)
 })
- 
+
 handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref);
     run_cmd('sh', ['./deploy.sh'], function(text){ console.log(text) });
 })
- 
-/*
-handler.on('issues', function (event) {
-  console.log('Received an issue event for % action=%s: #%d %s',
-    event.payload.repository.name,
-    event.payload.action,
-    event.payload.issue.number,
-    event.payload.issue.title)
-})
-*/
+
+try {
+  http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+      res.statusCode = 404
+      res.end('no such location')
+    })
+  }).listen(6666)
+}catch(err){
+  console.error('Error:', err.message)
+}
